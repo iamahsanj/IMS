f0f4950c5897b903b43e9dd4fd2d9206d2aab0d4
aj160 <ahsan-jamal@hotmail.com>
2020-05-11 01:41:18 +0500

    Initial Commit

---
+++ aspnet-core/src/IMS.Application/Users/IUserAppService.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using IMS.Roles.Dto;
+using IMS.Users.Dto;
+
+namespace IMS.Users
+{
+    public interface IUserAppService : IAsyncCrudAppService<UserDto, long, PagedUserResultRequestDto, CreateUserDto, UserDto>
+    {
+        Task<ListResultDto<RoleDto>> GetRoles();
+
+        Task ChangeLanguage(ChangeUserLanguageDto input);
+
+        Task<bool> ChangePassword(ChangePasswordDto input);
+    }
+}
